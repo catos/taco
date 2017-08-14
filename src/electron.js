79ffeef39ce5891763b1828007c298cf28dce475
Cato Skogholt <cskogholt@gmail.com>
2017-08-14 15:49:26 +0200

    Vue & Material Design Lite

---
+++ src/electron.js
@@ -0,0 +1,61 @@
+const path = require('path')
+
+// ------------------------------------------------------------
+
+const electron = require('electron')
+const { app, clipboard, globalShortcut, Menu, Tray, BrowserWindow } = electron
+
+// ------------------------------------------------------------
+
+const appMenu = require('./menu')
+const appTray = require('./tray')
+const appClipboard = require('./clipboard')
+
+// ------------------------------------------------------------
+
+let mainWindow
+
+app.on('ready', _ => {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800
+    })
+
+    mainWindow.openDevTools()
+
+    // mainWindow.loadURL(`file://${__dirname}/capture/capture.html`);
+    // mainWindow.loadURL(`file://${__dirname}/gitstatus/gitstatus.html`);
+    // mainWindow.loadURL(`file://${__dirname}/filelist/filelist.html`);
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    
+    mainWindow.on('close', _ => {
+        mainWindow = null
+    })
+
+    globalShortcut.register('CommandOrControl+Alt+D', _ => {
+        mainWindow.webContents.send('capture', app.getPath('pictures'))
+    })
+
+    appMenu.buildAndSet()
+    appTray.create();
+
+    let stack = []
+    appClipboard.checkForChange(clipboard, text => {
+        stack = appClipboard.addToStack(text, stack)
+        appClipboard.registerShortcuts(globalShortcut, clipboard, stack)
+
+        appMenu.template[1].submenu = appClipboard.menuTemplate(clipboard, stack)
+        appMenu.buildAndSet()
+
+        console.log('stack', stack);
+    })
+})
+
+app.on('will-quit', _ => {
+    console.log('will-quit!')
+    globalShortcut.unregisterAll()
+})
+
+app.on('quit', _ => {
+    console.log('quit!')
+})
--- src/filelist/filelist.js
+++ src/filelist/filelist.js
@@ -1,60 +1,129 @@
-const electron = require('electron')
 const fs = require('fs')
 const path = require('path')
 
-function updateFileList(dir, cb) {
-
-    if (!dir) {
-        dir = 'c:\\'
+const DELIMITER = '\\'
+const PARENT_DIRECTORY = {
+    order: -1,
+    name: '..',
+    stats: {
+        isDirectory: true
     }
-    console.log('dir', dir);
-    var result = ""
-    fs.readdir(dir, (err, files) => {
-        if (err) {
-            throw err;
+}
+
+Vue.component('filelist', {
+    // template: '#filelist',
+    template: `
+        <div class="filelist">
+            <div class="mdl-textfield mdl-js-textfield">
+                <input id="path" class="mdl-textfield__input" type="text" v-model="path" @keyup.enter="getFiles()" />
+                <label class="mdl-textfield__label" for="path">Path</label>
+            </div>                
+            <table class="mdl-data-table mdl-js-data-table">
+                <tr class="file" v-bind:class="{ folder: file.stats.isDirectory }" v-for="file in sortedFiles" @click="gotoFolder(file.name)">
+                    <td class="mdl-data-table__cell--non-numeric">{{ file.name }}</td>    
+                    <td class="size">{{ file.stats.size | kbSize }}</td>
+                    <td class="created">{{ file.stats.created | toShortDate }}</td>
+                </tr>
+            </table>
+        </div>
+    `,
+    data: function () {
+        return {
+            path: DELIMITER,
+            files: [PARENT_DIRECTORY]
         }
+    },
+    created: function () {
+        this.getFiles()
+    },
+    computed: {
+        sortedFiles: function () {
 
-        for (let file of files) {
-            let label = file
-
-            result += '<div'
-            if (isDir(path.join(dir, file))) {
-                result += ' class="directory"'
-                label = '/' + file
+            var cmp = function (a, b) {
+                if (a > b) return +1;
+                if (a < b) return -1;
+                return 0;
             }
-            result += '>'
-            result += label
-            result += '</div>'
+
+            return this.files
+                .sort(function (a, b) {
+                    return cmp(a.order, b.order) || cmp(a.name, b.name)
+                })
         }
-        document.getElementById('tree-left').innerHTML = result
-    });
+    },
+    methods: {
+        gotoFolder: function (folder) {
+            if (folder === '..') {
+                this.path = this.path.substring(0, this.path.lastIndexOf(DELIMITER))
+            } else {
+                this.path = path.join(this.path, folder)
+            }
 
-    cb()
-}
+            if (!this.path.length) {
+                this.path = DELIMITER
+            }
 
-function isDir(dir) {
-    try {
-        return fs.lstatSync(dir).isDirectory()
-    } catch (e) {
-        return false
-    }
-}
+            this.getFiles()
+        },
+        getFiles: function () {
+            var result = [PARENT_DIRECTORY]
 
-document.getElementById('dir').addEventListener('keyup', event => {
-    const dir = event.target.value.trim()
-    if (event.code === 'Enter' && isDir(dir)) {
-        updateFileList(dir)
+            fs.readdir(this.path, (err, files) => {
+                if (err) {
+                    throw err;
+                }
+
+                for (let file of files) {
+                    var stats = this.fileStats(path.join(this.path, file))
+
+                    var entry = {
+                        order: 1,
+                        name: file,
+                        stats: stats
+                    }
+
+                    if (stats.isDirectory)
+                        entry.order = 0
+
+                    result.push(entry)
+                }
+            })
+            this.files = result
+        },
+        fileStats: function (dir) {
+            try {
+                var stats = fs.lstatSync(dir)
+
+                return {
+                    isDirectory: stats.isDirectory(),
+                    size: stats.size,
+                    created: stats.ctime.toISOString()
+                }
+            } catch (e) {
+                return false
+            }
+        },
+    },
+    filters: {
+        kbSize: function (value) {
+            if (!value)
+                return ''
+
+            var result = Math.floor(value / 1024)
+
+            if (result < 1)
+                result = 1
+
+            return result + ' KB'
+        },
+        toShortDate: function (value) {
+            if (!value)
+                return ''
+
+            if (value.length < 10)
+                return value
+
+            return value.slice(0, 10)
+        }
     }
 })
-
-// Array.from(document.getElementsByClassName('directory')).forEach(function (item, index, array) {
-//     console.log(item)
-//     item.addEventListener('mouseup', event => {
-//         const dir = event.target.value.trim()
-//         console.log('dir', dir)
-//         updateFileList(dir)
-//     })
-// })
-
-
-updateFileList()
